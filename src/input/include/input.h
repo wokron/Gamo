cd74550c5ac6eb0fecf8ad2bcad9ad14bc7dba59
wokron <stringcatwok@gmail.com>
2023-07-17 19:15:02 +0800

    feat: input system (mouse and keyboard)

---
+++ src/input/include/input.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "utils.h"
+#include <SDL2/SDL.h>
+
+namespace gamo
+{
+    class Input
+    {
+    private:
+        bool _quit = false;
+        uint8_t *_keyboard_down = nullptr;
+        uint8_t *_keyboard = nullptr;
+        uint8_t *_keyboard_up = nullptr;
+        uint32_t _mouse_down;
+        uint32_t _mouse;
+        uint32_t _mouse_up;
+        int _mouse_x, _mouse_y;
+
+        static Input *_instance;
+
+    public:
+        static Input *GetInstance()
+        {
+            if (_instance == nullptr)
+            {
+                _instance = new Input();
+            }
+            return _instance;
+        }
+
+        bool Quit() { return _quit; }
+
+        /// @brief update the input states of keyboard and mouse,
+        /// this function should be called in every game step
+        void UpdateInputStates();
+
+        /// @brief check if the key with the scancode is being pressed now
+        /// @param scancode the scancode of the key
+        /// @return if the key is pressed
+        bool GetKey(SDL_Scancode scancode);
+
+        /// @brief check if the key with the scancode has just been pressed
+        /// @param scancode the scancode of the key
+        /// @return if the key has just been pressed
+        bool GetKeyDown(SDL_Scancode scancode);
+
+        /// @brief check if the keyboard keys has been released
+        /// @param scancode the scancode of the key
+        /// @return if the keyboard key has been released
+        bool GetKeyUp(SDL_Scancode scancode);
+
+        /// @brief check if the mouse button with the button number is being pressed now
+        /// @param scancode the button number
+        /// @return if the button is pressed
+        bool GetMouseButton(int button);
+
+        /// @brief check if the mouse button with the button number has just been pressed
+        /// @param scancode the button number
+        /// @return if the button has just been pressed
+        bool GetMouseButtonDown(int button);
+
+        /// @brief check if the mouse button with the button number has been released
+        /// @param scancode the button number
+        /// @return if the button has been released
+        bool GetMouseButtonUp(int button);
+
+        /// @brief get the current mouse position,
+        /// regardless of whether the mouse is shown or not.
+        /// null can be passed for param x and y
+        /// @return the positon of the mouse, relative to the focus window
+        void GetMousePosition(int *x, int *y);
+
+    private:
+        Input();
+        ~Input();
+        Input(const Input &);
+        Input &operator=(const Input &);
+    };
+
+} // namespace gamo
---
+++ src/input/src/input.cpp
@@ -0,0 +1,99 @@
+#include "input.h"
+#include <SDL2/SDL.h>
+
+namespace gamo
+{
+    Input *Input::_instance = nullptr; // init static member
+
+    Input::Input()
+    {
+        int num_keys;
+        SDL_PumpEvents();                // update the state state
+        SDL_GetKeyboardState(&num_keys); // just get the array length
+        _keyboard = new uint8_t[num_keys];
+        _keyboard_up = new uint8_t[num_keys];
+        _keyboard_down = new uint8_t[num_keys];
+    }
+
+    Input::~Input()
+    {
+        delete _keyboard;
+        delete _keyboard_up;
+        delete _keyboard_down;
+    }
+
+    void Input::UpdateInputStates()
+    {
+        _quit = false;
+        
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                _quit = true;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        int num_keys;
+        auto cur_keyboard = SDL_GetKeyboardState(&num_keys);
+        for (int i = 0; i < num_keys; i++)
+        {
+            _keyboard_down[i] = !_keyboard[i] && cur_keyboard[i] ? 1 : 0;
+            _keyboard_up[i] = _keyboard[i] && !cur_keyboard[i] ? 1 : 0;
+            _keyboard[i] = cur_keyboard[i];
+        }
+
+        auto cur_mouse = SDL_GetMouseState(&_mouse_x, &_mouse_y);
+        _mouse_down = ~_mouse & cur_mouse;
+        _mouse_up = _mouse & ~cur_mouse;
+        _mouse = cur_mouse;
+    }
+
+    bool Input::GetKey(SDL_Scancode scancode)
+    {
+        return _keyboard[scancode] == 1;
+    }
+
+    bool Input::GetKeyDown(SDL_Scancode scancode)
+    {
+        return _keyboard_down[scancode] == 1;
+    }
+
+    bool Input::GetKeyUp(SDL_Scancode scancode)
+    {
+        return _keyboard_up[scancode] == 1;
+    }
+
+    bool Input::GetMouseButton(int button)
+    {
+        return _mouse & SDL_BUTTON(button);
+    }
+
+    bool Input::GetMouseButtonDown(int button)
+    {
+        return _mouse_down & SDL_BUTTON(button);
+    }
+
+    bool Input::GetMouseButtonUp(int button)
+    {
+        return _mouse_up & SDL_BUTTON(button);
+    }
+
+    void Input::GetMousePosition(int *x, int *y)
+    {
+        if (x != nullptr)
+        {
+            *x = _mouse_x;
+        }
+        if (y != nullptr)
+        {
+            *y = _mouse_y;
+        }
+    }
+} // namespace gamo
