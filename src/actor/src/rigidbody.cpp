2346c117057b153f57176873f51b651dc705cbe3
wokron <stringcatwok@gmail.com>
2023-07-24 16:11:18 +0800

    fix: sdl and box2d use diff rotation direction

--- src/actor/src/rigidbody.cpp
+++ src/actor/src/rigidbody.cpp
@@ -21,7 +21,8 @@ namespace gamo
         _bodydef.userData.pointer = (uintptr_t)this;
         _body = world->CreateBody(&_bodydef);
         auto pos = GetTransform()->Position();
-        auto angle = GetTransform()->Rotate() * M_PI / 180;
+        // sdl and box2d use diff rotation direction, so negative is needed
+        auto angle = -GetTransform()->Rotate() * M_PI / 180;
         _body->SetTransform({pos.x, pos.y}, angle);
         UnregisterHandleInit();
     }
@@ -33,7 +34,9 @@ namespace gamo
         {
             auto tmp = transform->Position();
             Vec2 pos(tmp.x, tmp.y);
-            _body->SetTransform(pos, transform->Rotate() * M_PI / 180);
+            // sdl and box2d use diff rotation direction, so negative is needed
+            float angle = -transform->Rotate() * M_PI / 180; 
+            _body->SetTransform(pos, angle);
         }
     }
 
@@ -43,7 +46,8 @@ namespace gamo
         auto angle = _body->GetAngle();
         auto transform = GetTransform();
         transform->Position({pos.x, pos.y});
-        transform->Rotate(angle / M_PI * 180);
-        transform->IsPositionOrRotateModified();
+        // sdl and box2d use diff rotation direction, so negative is needed
+        transform->Rotate(-angle / M_PI * 180);
+        transform->IsPositionOrRotateModified(); // reset flag
     }
 } // namespace gamo
