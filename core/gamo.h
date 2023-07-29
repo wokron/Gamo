c3e2f53aef6763a9527ef968b8f8479523a5acac
wokron <stringcatwok@gmail.com>
2023-07-29 18:29:59 +0800

    chore: config for pybind

---
+++ core/gamo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "actor/include/actor.h"
+#include "director/include/director.h"
+#include "event/include/event.h"
+#include "game/include/game.h"
+#include "input/include/input.h"
+#include "physics/include/physics.h"
+#include "render/include/render.h"
+#include "utils/include/utils.h"
