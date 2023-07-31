1d3a1f5f290740ee9a1b18cd34b57bfc0c057c60
wokron <stringcatwok@gmail.com>
2023-07-31 14:37:13 +0800

    test: test singleton template

---
+++ core/utils/test/test_singleton_template.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "singleton.h"
+
+SINGLETON_BEGIN(MySingleton)
+public:
+    int value = 0;
+    bool started = false;
+
+    void Init() { started = true; }
+    void Destroy() { started = false; }
+SINGLETON_END
+
+TEST(TestSingletonTemplate, test_singleton)
+{
+    auto a = MySingleton::GetInstance();
+    ASSERT_NE(a, nullptr);
+    auto b = MySingleton::GetInstance();
+    ASSERT_NE(b, nullptr);
+    ASSERT_EQ(a, b);
+
+    a->value = 10;
+    ASSERT_EQ(b->value, 10);
+
+    ASSERT_FALSE(b->started);
+    a->Init();
+    ASSERT_TRUE(b->started);
+    b->Destroy();
+    ASSERT_FALSE(a->started);
+}
