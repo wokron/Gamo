ed8deceb18f90635a8ed44fce519053ecec34935
wokron <stringcatwok@gmail.com>
2023-09-01 16:19:22 +0800

    test: test prototype template

---
+++ core/utils/test/test_prototype_template.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include "prototype.h"
+
+using namespace gamo;
+
+class MyClass : public IPrototype<MyClass>
+{
+public:
+    int val1 = 0;
+    int val2 = 0;
+
+    MyClass(int val1) : val1(val1) {}
+
+    MyClass *Clone() override
+    {
+        return new MyClass(val1);
+    }
+};
+
+TEST(TestPrototypeTemplate, test_prototype)
+{
+    auto obj = new MyClass(123);
+    ASSERT_EQ(obj->val1, 123);
+    ASSERT_EQ(obj->val2, 0);
+
+    auto obj2 = obj->Clone();
+    ASSERT_NE(obj, obj2);
+    ASSERT_EQ(obj2->val1, 123);
+    ASSERT_EQ(obj2->val2, 0);
+
+    obj->val1 = 12;
+    ASSERT_EQ(obj->val1, 12);
+    ASSERT_EQ(obj->val2, 0);
+    ASSERT_EQ(obj2->val1, 123);
+    ASSERT_EQ(obj2->val2, 0);
+
+}
